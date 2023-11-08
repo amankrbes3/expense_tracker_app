42747756dfb978505e44dcdbf12aa63f0b3b223a
Aman Kumar <amankrbes3@gmail.com>
2023-11-09 00:01:12 +0530

    Uploading project from local device

---
+++ lib/expense_tracker.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+class ExpenseTracker extends StatefulWidget{
+  @override
+  State<ExpenseTracker> createState(){
+    return _ExpenseTrackerState();
+  }
+}
+
+class _ExpenseTrackerState extends State<ExpenseTracker>{
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Expense Tracker App"),
+      ),
+    );
+  }
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:expense_tracker_app/expense_tracker.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: ExpenseTracker(),
+  ));
+}
+
+
